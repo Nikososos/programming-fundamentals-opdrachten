a38b7a58843e3017ccec034aa167153fd76c7c72
Nikososos <nickywolf@live.nl>
2025-05-16 17:03:53 +0200

    Deel 1 van het verhaal toegevoegd aan project tot eerste beslissing

--- oefenopdrachten/oefenopdracht-1.py
+++ oefenopdrachten/oefenopdracht-1.py
@@ -2,3 +2,12 @@
 # Vraag de gebruiker om input voor de variabelen
 # Bonus geef de gebruiker keuze als een make your own story
 
+naam = input("Wat is jouw naam?: ")
+
+print(f"In het hart van het donkerwoud vond {naam} een glinsterende sleutel tussen de wortels van een... ")
+
+boom = input("Je moet me een beetje helpen bij dit verhaal, bij welke boom vond je de sleutel?: ")
+
+print(f"aah juist tussen de wortels van een {boom}. Nieuwsgierig volgt {naam} een verborgen pad dat alleen zichtbaar werd in het maanlicht. Aan het einde ziet {naam} een...")
+
+bestemming = input("Wat zie je in de verte, een kasteel of een ")
