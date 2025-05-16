54d89573dc6ccbf0ad953ad797a77fa0eb40fe95
Nikososos <nickywolf@live.nl>
2025-05-16 16:35:00 +0200

    Voeg de beschrijving toe aan oefenopdracht-1

---
+++ oefenopdrachten/oefenopdracht-1.py
@@ -0,0 +1,4 @@
+# Schrijf een verhaal van ongeveer 20-50 woorden waarin je minimaal 5 variabelen zet die de gebruiker zelf moet invullen
+# Vraag de gebruiker om input voor de variabelen
+# Bonus geef de gebruiker keuze als een make your own story
+
