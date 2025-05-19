11fe10d8d581d86f53c5d8f9e970ccd7418f6324
Nikososos <nickywolf@live.nl>
2025-05-19 20:33:04 +0200

    Verhaal afgemaakt met de laatste 3 inputs en afgeschreven

--- oefenopdrachten/oefenopdracht-1.py
+++ oefenopdrachten/oefenopdracht-1.py
@@ -1,6 +1,5 @@
 # Schrijf een verhaal van ongeveer 20-50 woorden waarin je minimaal 5 variabelen zet die de gebruiker zelf moet invullen
 # Vraag de gebruiker om input voor de variabelen
-# Bonus geef de gebruiker keuze als een make your own story
 
 naam = input("Wat is jouw naam?: ")
 
@@ -10,4 +9,16 @@ boom = input("Je moet me een beetje helpen bij dit verhaal, bij welke boom vond 
 
 print(f"aah juist tussen de wortels van een {boom}. Nieuwsgierig volgt {naam} een verborgen pad dat alleen zichtbaar werd in het maanlicht. Aan het einde ziet {naam} een...")
 
-bestemming = input("Wat zie je in de verte, een kasteel of een ")
+pad_kleur = input("Het pad had een hele aparte kleur, wat voor kleur had dit pad?: ")
+
+print(f"Het pad was {pad_kleur} gekleurd en aan het einde hiervan zag {naam} een vergeten poort vergroeid met een klimop")
+
+bloem_kleur = input(f"Toen {naam} de sleutel in de poort deed op zicht een magische tuin vol met gekleurde bloemen. Welke kleur waren deze bloemen?: ")
+
+print(f"De {bloem_kleur}gekleurde bloemen straalde mooi in het {pad_kleur}gekleurde licht. Plots sprak er een wijze uil...")
+
+uil = input(f"Wat voor een soort sprak er?: ")
+
+print(f"De wijze {uil} zei: 'Je hebt het pad van het hart gevolgd, hier begint jouw echte avontuur")
+print(f"{naam} glimlacht en weet dat hierna nooit meer iets hetzelfde zal zijn.")
+print("HET EINDE")
